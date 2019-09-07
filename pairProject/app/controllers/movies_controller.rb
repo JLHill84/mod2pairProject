c90efd2fc87b2206e9d3a1fee14af9dc310c2187
github username <github email address>
2019-09-07 17:23:29 -0500

    Movie creation

--- pairProject/app/controllers/movies_controller.rb
+++ pairProject/app/controllers/movies_controller.rb
@@ -8,4 +8,23 @@ class MoviesController < ApplicationController
         @movie = Movie.find(params[:id])
     end
 
+    def new
+        :new
+    end
+
+    def create
+        @movie = Movie.create({
+            title: params[:title],
+            description: params[:description],
+            mpaa: params[:mpaa],
+            released: params[:released],
+            posterURL: params[:posterURL],
+            trailerURL: params[:trailerURL],
+            stars: params[:stars]
+        })
+        if @movie.save
+            redirect_to "/movies/#{@movie.id}", :notice => "Your movie was saved!"
+        end
+    end
+
 end
