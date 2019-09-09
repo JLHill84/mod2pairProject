c4de89382e405afb2100b5802d58797e375db099
github username <github email address>
2019-09-09 17:36:20 -0500

    More seeds, and login work

---
+++ pairProject/app/controllers/login_controller.rb
@@ -0,0 +1,14 @@
+class LoginController < ApplicationController
+
+    def index
+
+    end
+
+    def login
+        @user =  User.find_by(userName: params[:userName])
+        if @user.password_digest == Bcrypt::Password(params[:password])
+            session[:current_user] = @user
+        end
+    end
+
+end
--- pairProject/db/seeds.rb
+++ pairProject/db/seeds.rb
@@ -90,6 +90,26 @@ posterURL: "https://www.joblo.com/assets/images/oldsite/newsimages1/102141.jpg",
 trailerURL: "FKwtGHbpVDU"
 })
 
+Movie.create({
+    title: "Dragonball Evolution",
+    description: "On his 18th birthday, Goku (Justin Chatwin) receives a mystical Dragonball as a gift from his grandfather. There are only six others like it in the whole world, and legend has it that whomever possesses all seven will be granted one perfect wish. When the arrival of a dark force triggers a tragedy, Goku and his companions (Chow Yun-Fat, Emmy Rossum) are propelled into an epic quest to collect the seven Dragonballs and save the Earth from destruction.",
+    mpaa: "PG",
+    stars: 1,
+    released: 2009,
+    posterURL: "https://image.tmdb.org/t/p/original/yERkcVylXAcTCi8f0qrba4rmWwg.jpg",
+    trailerURL: "T4_wHfz2qUA"
+})
+
+Movie.create({
+    title: "Justice League",
+    description: "Fuelled by his restored faith in humanity, and inspired by Superman's selfless act, Bruce Wayne enlists newfound ally Diana Prince to face an even greater threat. Together, Batman and Wonder Woman work quickly to recruit a team to stand against this newly-awakened enemy. Despite the formation of an unprecedented league of heroes in Batman, Wonder Woman, Aquaman, Cyborg and the Flash, it may be too late to save the planet from an assault of catastrophic proportions.",
+    mpaa: "PG-13",
+    stars: 2,
+    released: 2017,
+    posterURL: "https://m.media-amazon.com/images/M/MV5BYWVhZjZkYTItOGIwYS00NmRkLWJlYjctMWM0ZjFmMDU4ZjEzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_SX675_AL_.jpg",
+    trailerURL: "g_6yBZKj-eo"
+})
+
 User.create({
     name: "Billy Jean",
     userName: "notMy",
